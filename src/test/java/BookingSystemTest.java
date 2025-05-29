a788e15d7742b238bb1d523458af7164f44634a9
alistanikzay <ali.kawsar.stanikzay>
2025-05-29 20:29:18 +0200

    some changes + test failed

--- src/test/java/BookingSystemTest.java
+++ src/test/java/BookingSystemTest.java
@@ -7,7 +7,10 @@ import org.mockito.MockitoAnnotations;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
+
 
 public class BookingSystemTest {
 
