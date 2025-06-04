a00dace6740a46c2fce230331162cb10b3a3f169
alistanikzay <ali.kawsar.stanikzay@iths.se>
2025-06-04 15:16:39 +0200

    Gör om del 2 så TDD följer

--- src/main/java/org/example/ShoppingCart.java
+++ src/main/java/org/example/ShoppingCart.java
@@ -6,38 +6,5 @@ import java.util.List;
 public class ShoppingCart {
     private final List<Item> items = new ArrayList<>();
 
-    public void addItem(String name, double price, int quantity) {
 
-        for (Item item : items) {
-            if (item.getName().equals(name)) {
-                item.setQuantity(item.getQuantity() + quantity); // Uppdatera kvantiteten
-                return;
-            }
-        }
-
-        items.add(new Item(name, price, quantity));
-    }
-
-    public void removeItem(String name) {
-
-        items.removeIf(item -> item.getName().equals(name));
-    }
-
-    public List<Item> getItems() {
-        return items;
-    }
-
-    public double calculateTotal() {
-        double total = 0;
-        for (Item item : items) {
-            total += item.getPrice() * item.getQuantity();
-        }
-        return total;
-    }
-
-    public double applyDiscount(double percentage) {
-        double total = calculateTotal();
-        double discountAmount = total * (percentage / 100);
-        return total - discountAmount;
-    }
 }
