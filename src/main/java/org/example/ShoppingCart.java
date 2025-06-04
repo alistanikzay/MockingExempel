91081e26b47a49bc7dfe17fc53d84c17ad3ff0fe
alistanikzay <ali.kawsar.stanikzay@iths.se>
2025-06-04 16:03:44 +0200

    Gör om del 2 så TDD följs. 2ND test failed. Added and updated method in ShoppingCart. 2ND test became Green

--- src/main/java/org/example/ShoppingCart.java
+++ src/main/java/org/example/ShoppingCart.java
@@ -7,8 +7,15 @@ public class ShoppingCart {
     private final List<Item> items = new ArrayList<>();
 
     public void addItem(String name, double price, int quantity) {
+        for (Item item : items) {
+            if (item.getName().equals(name)) {
+                item.setQuantity(item.getQuantity() + quantity);
+                return;
+            }
+        }
         items.add(new Item(name, price, quantity));
     }
+
     public List<Item> getItems() {
         return items;
     }
